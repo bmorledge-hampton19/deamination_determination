b1b510e2aafa11e80f876189876b0cc1adcdfafc
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2022-12-15 11:45:16 -0800

    Create FilterNonstandardNucleotides.py
    Filters a simple text file where each line contains a nucleotide sequence. Sequences with nonstandard nucleotides (i.e. any except A, C, G, T, and N) are removed from the output.

---
+++ python/FilterNonstandardNucleotides.py
@@ -0,0 +1,37 @@
+# Filters a simple text file where each line contains a nucleotide sequence.
+# Sequences with nonstandard nucleotides (i.e. any except A, C, G, T, and N) are removed from the output.
+import os
+from typing import List
+from benbiohelpers.TkWrappers.TkinterDialog import TkinterDialog
+nucleotides = {'A','C','G','T','N'}
+
+
+def filterNonstandardNucleotides(inputFilePaths: List[str]):
+
+    outputFilePaths: List[str] = list()
+
+    for inputFilePath in inputFilePaths:
+
+        print(f"Working with {os.path.basename(inputFilePath)}...")
+
+        outputFilePath = inputFilePath.rsplit('.', 1)[0] + "_nonstandard_nuc_filtered.txt"
+        outputFilePaths.append(outputFilePath)
+
+        with open(inputFilePath, 'r') as inputFile:
+            with open(outputFilePath, 'w') as outputFile:
+                for line in inputFile:
+                    if nucleotides.issuperset(line.strip()): outputFile.write(line)
+                    else: print(line.strip())
+
+    return outputFilePaths
+
+
+def main():
+
+    with TkinterDialog(workingDirectory = os.path.join(__file__,"..")) as dialog:
+        dialog.createMultipleFileSelector("Simple text nucleotide files:", 0, "simple_nuc.txt", ("text files", ".txt"))
+
+    filterNonstandardNucleotides(dialog.selections.getFilePathGroups()[0])
+
+
+if __name__ == "__main__": main()
