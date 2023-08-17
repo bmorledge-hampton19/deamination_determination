d134c0e3e779dcc9b86ca10cf4b6c7dd00f077f7
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2023-08-17 14:59:57 -0700

    Custom breaks option for paired cut site fig gen

--- R/CutSiteCorrelationAnalysis.R
+++ R/CutSiteCorrelationAnalysis.R
@@ -61,7 +61,8 @@ getReadLengthSlope = function(pairedCutSiteDistances) {
 plotPairedCutSiteDistances = function(pairedCutSiteDistances, dependentVariable,
                                       xAxisLabel = "5' Cut Site Distance", yAxisLabel = NULL,
                                       title = "Paired Cut Site Distances", sizeRange = c(0.1,5),
-                                      xlim = NULL, ylim = NULL, xAxisBreaks = waiver(), yAxisBreaks = waiver()) {
+                                      xlim = NULL, ylim = NULL, xAxisBreaks = waiver(), yAxisBreaks = waiver(),
+                                      breaks = waiver()) {
 
   if (is.null(yAxisLabel)) {
     if (dependentVariable == THREE_PRIME_CUT_SITE_DISTANCE) {
@@ -72,10 +73,10 @@ plotPairedCutSiteDistances = function(pairedCutSiteDistances, dependentVariable,
   }
 
   plot = ggplot(pairedCutSiteDistances, aes(x = Five_Prime_Cut_Site_Distance, y = !!sym(dependentVariable))) +
-    geom_count() + scale_size(range = sizeRange) + blankBackground + defaultTextScaling +
+    geom_count() + scale_size(range = sizeRange) + scale_size_continuous(breaks = breaks) +
     geom_smooth(method = "lm", formula = y~x, color = "red2", se = FALSE) +
     labs(title = title, x = xAxisLabel, y = yAxisLabel) + coord_cartesian(xlim = xlim, ylim = ylim, expand = FALSE) +
-    scale_x_continuous(breaks = xAxisBreaks) + scale_y_continuous(breaks = yAxisBreaks)
+    scale_x_continuous(breaks = xAxisBreaks) + scale_y_continuous(breaks = yAxisBreaks) + blankBackground + defaultTextScaling
 
   print(plot)
 
